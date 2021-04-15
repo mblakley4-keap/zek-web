3a8e3c4ba0bee9ff5dbc030459eaf48c4307cc20
Mike Blakley <mike.blakley@mikeblakleyml1.attlocal.net>
2021-04-15 13:45:48 -0400

    init setup

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="logo" class="logo" src="./assets/keapK.png">
+    <HelloWorld msg="Welcome to Mike's App"/>
   </div>
 </template>
 
@@ -24,5 +24,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.logo {
+  height: 150px;
 }
 </style>
